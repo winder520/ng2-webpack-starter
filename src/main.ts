64f253a3b9d522d24be878711f8576f655b53866
winder <winder@kopbit.com>
2017-05-31 18:10:27 +0800

    优化代码

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,7 @@ import { AppModule } from './app/app.module';
 import { SSO } from './lib/sso/sso';
 import { Util } from './lib/util/util';
 
-
-//require('./styles.css');
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
-// getTranslationProviders().then(providers => {
-
-//   platformBrowserDynamic().bootstrapModule(AppModule, { providers })
-// })
 platformBrowserDynamic().bootstrapModule(AppModule)
-  // .then(success => console.log('Bootstrap success'))
-  // .catch(err => console.error(err));
